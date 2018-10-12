8b6ee77cce04bef692cf93cefad3382f18c90d9c
ken <flying861110@foxmail.com>
2018-10-12 08:31:25 +0800

    cas services config

--- CAS52/src/main/java/com/wey/framework/cas/pojo/User.java
+++ CAS52/src/main/java/com/wey/framework/cas/pojo/User.java
@@ -4,6 +4,10 @@ import java.io.Serializable;
 
 public class User implements Serializable {
     
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
     private Long id;
     private String username;
     private String password;
